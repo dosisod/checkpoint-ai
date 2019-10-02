a159a1c1eeaf4eda791557cc4ed84a42ec1ebb11
dosisod <39638017+dosisod@users.noreply.github.com>
2019-10-01 21:38:11 -0700

    added color displaying of cube

--- examples/rubiks_cube/rubiks.py
+++ examples/rubiks_cube/rubiks.py
@@ -1,5 +1,7 @@
-from checkpoint_ai import CheckpointAlgo, Checkpoint
+from colorama import Back
 import math
+
+from checkpoint_ai import CheckpointAlgo, Checkpoint
 
 """ faces affected by turning
 W Y G B R O: U
@@ -24,11 +26,21 @@ W Y G B R O: B
 class Cube:
 	def __init__(self):
 		#cube is orientated with white up, and red facing user
-		self.cubes=[]
-		self.faces="W","Y","G","B","R","O"
+		self.faces=[]
 
-		for index, name in enumerate(self.faces):
-			self.cubes.append([[[name]*3]*3]) #makes 3 rows of 3 columns (one face)
+		#id and color table:
+		self.colors=(
+			Back.WHITE,   #0
+			Back.GREEN,   #1
+			Back.RED,     #2
+			Back.BLUE,    #3
+			Back.MAGENTA, #4  #orange cannot be printed (by default), use magenta instead
+			Back.YELLOW   #5
+		)
+
+		#for index, name in enumerate(self.faces):
+		for index in range(6):
+			self.faces.append([[index]*3]*3) #makes 3 rows of 3 columns (one face)
 
 	def rotate(self, move): #pass a move like "U2" or "F"
 		move=move.upper()
@@ -37,7 +49,7 @@ class Cube:
 		#these 3 if's only rotate adjecent sides, not face
 		if (move[0]=="U" or move[0]=="D"):
 			for i in a:
-				self.cubes
+				pass
 
 	def parse(self, move): #parse between n2, n, and n'
 		move=move[1:] #removes trailing character
@@ -60,8 +72,30 @@ class Cube:
 	def relative(self, parent, child): #find where child is relative to parent
 		pass
 
+	def display(self): #print the current state of the cube out
+		rows=[] #stores faces to be drawn
+
+		rows+=self.faces[0] #add each white layer
+
+		for row in range(3):
+			for face in range(4):
+				rows.append(self.faces[face+1][row])
+
+		rows+=self.faces[5] #add each yellow layer
+
+		for index, row in enumerate(rows):
+			ii=index//3
+			if ii==0 or ii==5:
+				print("   ", end="")
+
+			for pindex, piece in enumerate(row):
+				print(self.colors[piece]+" "+Back.RESET, end="")
+
+			if ii==0 or ii==5 or (ii!=0 and ii!=5 and index%4==2):
+				print("")
+
 if __name__=="__main__":
 	AI=CheckpointAlgo([])
 	
 	cube=Cube()
-	print(cube.cubes)
+	cube.display()
